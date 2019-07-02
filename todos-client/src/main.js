02eb1db5d4aad0825890cfd87149d395853d7339
kimdogyu <rnrdkxoa1@gmail.com>
2019-07-02 12:39:24 +0900

    추가,삭제 rest api 이용전

--- todos-client/src/main.js
+++ todos-client/src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//axios 사용
+import axios from 'axios'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
--- todos-client/src/router/index.js
+++ todos-client/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Example from '@/components/Example'
+import TodoPage from '@/components/TodoPage'
+
 
 Vue.use(Router)
 
@@ -16,6 +18,11 @@ export default new Router({
       path: '/example',
       name: 'Example',
       component: Example
+    },
+    {
+      path: '/todos',
+      name: 'TodoPage',
+      component: TodoPage
     }
   ]
 });
